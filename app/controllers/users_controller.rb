78a021fc32cc5e1fb97056988fdde4af4b0ff23c
harsha <katikiharsha@gmail.com>
2016-03-10 12:48:55 -0500

    added new file to the project

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
 class UsersController < ApplicationController
+	before_action :load_user ,  :only[:show , :edit]
   def index
+  	@user=User.all
   end
+
+  def show
+  
+  end
+
+  def edit
+  	
+  end
+
+  def load_user
+  	@user=User.find(params[:id])
+  end
+  
+
 end
