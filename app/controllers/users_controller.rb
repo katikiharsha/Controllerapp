4de328a4a81d0fb4598f6e5614600d2cccfc7d3e
harsha <katikiharsha@gmail.com>
2016-03-10 15:07:58 -0500

    added new files and code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
-	before_action :load_user ,  :only[:show , :edit]
+	before_action :load_user ,  :only => [:show , :edit]
+
   def index
   	@user=User.all
+  end
+
+  def new
+  	@user=User.new
+  end 
+
+  def create
+  	@user=User.new(params.require(:user).permit(:email , :phone))
+  	if @user.save
+  		redirect_to :action => "index"
+  	else
+  		render "new"
+  	end
   end
 
   def show
@@ -11,6 +25,8 @@ class UsersController < ApplicationController
   def edit
   	
   end
+
+  private
 
   def load_user
   	@user=User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/index'
-  get 'users/show'
+  get 'users/show/:id' => "users#show"
+   get 'users/new'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
